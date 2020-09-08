45932a63b68f09124ffa7f36695876cfc28322c1
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-09-08 21:02:58 +0100

    Added validation with RHF

--- components/shared/PortfolioForm.js
+++ components/shared/PortfolioForm.js
@@ -1,62 +1,124 @@
+import { useForm } from 'react-hook-form'
+import { Alert } from 'react-bootstrap'
 
+// true -> input is valid
+// false -> input is NOT valid
+const firstLetterUpper = iText => {
+  if (!iText) { return true }
 
-const PortfolioForm = () => {
+  if (iText[0].toUpperCase() === iText[0]) {
+    return true
+  }
+
+  return false
+  // return iText[0].toUpperCase() === iText[0];
+}
+
+const PortfolioForm = ({ onSubmit }) => {
+
+  const { register, handleSubmit, errors } = useForm()
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group">
         <label htmlFor="title">Title</label>
         <input
+          ref={register({ required: true, maxLength: 30, validate: { firstLetterUpper }})}
           name="title"
           type="text"
           className="form-control"
-          id="title"/>
+          id="title" />
+        {
+          errors.title &&
+          <Alert variant="danger">
+            {errors.title.type == 'required' && <p>Title is required</p>}
+            {errors.title.type == 'maxLength' && <p>Maximum of 30characters</p>}
+            { errors.title.type === "firstLetterUpper" && <p>First letter should be uppercased!</p> }
+          </Alert>
+        }
       </div>
 
       <div className="form-group">
         <label htmlFor="city">Company</label>
         <input
+          ref={register({ required: true, minLength: 5 })}
           name="company"
           type="text"
           className="form-control"
-          id="company"/>
+          id="company" />
+        {
+          errors.company &&
+          <Alert variant="danger">
+            {errors.company.type == 'required' && <p>Company is required</p>}
+            {errors.company.type == 'minLength' && <p>Minimum of 5 characters</p>}
+          </Alert>
+        }
       </div>
 
       <div className="form-group">
         <label htmlFor="city">Company Website</label>
         <input
+          ref={register({ required: true, pattern: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ })}
           name="companyWebsite"
           type="text"
           className="form-control"
-          id="companyWebsite"/>
+          id="companyWebsite" />
+        {
+          errors.companyWebsite &&
+          <Alert variant="danger">
+            {errors.companyWebsite.type == 'required' && <p>Company's Website is required</p>}
+            {errors.companyWebsite.type === 'pattern' && <p>Enter a valid url!</p>}
+          </Alert>
+        }
       </div>
 
       <div className="form-group">
         <label htmlFor="street">Location</label>
         <input
+          ref={register({ required: true })}
           name="location"
           type="text"
           className="form-control"
-          id="location"/>
+          id="location" />
+        {
+          errors.location &&
+          <Alert variant="danger">
+            {errors.location.type == 'required' && <p>location is required</p>}
+          </Alert>
+        }
       </div>
 
       <div className="form-group">
         <label htmlFor="street">Job Title</label>
         <input
+          ref={register({ required: true })}
           name="jobTitle"
           type="text"
           className="form-control"
-          id="jobTitle"/>
+          id="jobTitle" />
+        {
+          errors.jobTitle &&
+          <Alert variant="danger">
+            {errors.jobTitle.type == 'required' && <p>job Title is required</p>}
+          </Alert>
+        }
       </div>
 
       <div className="form-group">
         <label htmlFor="description">Description</label>
         <textarea
+          ref={register({ required: true })}
           name="description"
           rows="5"
           type="text"
           className="form-control"
           id="description">
         </textarea>
+        {
+          errors.description &&
+          <Alert variant="danger">
+            {errors.description.type == 'required' && <p>Description is required</p>}
+          </Alert>
+        }
       </div>
       <button
         type="submit"
--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,12 +1,12 @@
-
+import React from 'react'
 
 const PortfolioDetail = () => {
   return (
-    <>
+    <React.Fragment>
       <div className="portfolio-detail">
         <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
           <main role="main" className="inner page-cover">
-            <>
+            <React.Fragment>
               <h1 className="cover-heading">Amazing Experience</h1>
               <p className="lead dates">27/12/2009 - 31/04/2011</p>
               <p className="lead info mb-0">Developer | Eincode | New York</p>
@@ -14,12 +14,12 @@ const PortfolioDetail = () => {
               <p className="lead">
                 <a href="#" target="_" className="btn btn-lg btn-secondary">Visit Company</a>
               </p>
-            </>
+            </React.Fragment>
           </main>
         </div>
       </div>
-    </>
+    </React.Fragment>
   )
 }
 
-export default PortfolioDetail;
+export default PortfolioDetail
